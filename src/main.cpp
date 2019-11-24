568a6544f2901622dfdc494d39f0027423013aaf
Renzo Crisostomo <renzo@crisostomo.me>
2019-11-24 17:19:22 +0100

    Add glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,38 @@
 #include "SDL.h" 
+#include "glad/glad.h"
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
-	SDL_Init(SDL_INIT_VIDEO);
-
-	SDL_Window* window = SDL_CreateWindow(
-		"SDL2Test",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		640,
-		480,
-		0
-	);
-
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-
-	SDL_Delay(3000);
-
-	SDL_DestroyWindow(window);
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
+		exit(1);
+	}
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+	SDL_Window* window = SDL_CreateWindow("main", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320, 460, SDL_WINDOW_OPENGL);
+	SDL_GLContext glContext = SDL_GL_CreateContext(window);
+	(void)glContext;
+	if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+		cout << "Failed to initialize the OpenGL context." << endl;
+		exit(1);
+	}
+	cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << endl;
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	bool quit = false;
+	while (!quit) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+		glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(window);
+	}
 	SDL_Quit();
-
 	return 0;
 }
